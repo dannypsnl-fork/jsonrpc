d218ec7072e3b2d3fa463222967b52d8f446a957
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-11 16:30:10 +0800

    fixed

--- server.rkt
+++ server.rkt
@@ -2,24 +2,28 @@
 
 (require web-server/servlet-env
          web-server/http/request-structs
-         web-server/http/json)
+         web-server/http/json
+         json)
 
 (define (start request)
-  (define json (request-post-data/raw request))
-  (cond
-    [(hash? json)
-     (response/jsexpr
-      (hasheq 'jsonrpc "2.0"
-              'id (hash-ref json 'id)
-              'result '()))]
-    [else
+  (define json-str (request-post-data/raw request))
+  (match json-str
+    [#f
      (response/jsexpr
       (hasheq 'jsonrpc "2.0"
               'id "0"
+              'result '()))]
+    [json-str
+     (define json (bytes->jsexpr json-str))
+     (displayln json)
+     (response/jsexpr
+      (hasheq 'jsonrpc "2.0"
+              'id (hash-ref json 'id)
               'result '()))]))
 
 (module+ main
   (serve/servlet
    start
    #:port 4389
+   #:launch-browser? #f
    #:servlet-path "/jsonrpc"))
