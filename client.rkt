28d48b69c6ce4f6a2b721406f5275d331bcc4621
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-11 12:10:14 +0800

    client

---
+++ client.rkt
@@ -0,0 +1,17 @@
+#lang racket/base
+
+(provide jsonrpc-send!)
+
+(require json
+         net/url)
+
+; send a hash to a jsonrpc server, receive a hash as response
+;
+; (jsonrpc-send! "http://127.0.0.1:8123/jsonrpc"
+;                (hasheq 'method "JSONRPC.Introspect" 'jsonrpc "2.0" 'id "0"))
+(define (jsonrpc-send! url payload)
+  (string->jsexpr
+   (read-line
+    (post-pure-port (string->url url)
+                    (string->bytes/utf-8 (jsexpr->string payload))
+                    '("Content-Type: application/json")))))
